3adf3e898b1086133936f5eaf37e1554aa62ba1c
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-01-26 20:10:41 +1100

    Moved node_modules to git ignore and continued working on button for notes app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import IconButton from '@mui/material';
+// import IconButton from '@mui/material';
 
 function App() {
   return (
     <div>
-      <IconButton aria-label="notes-app">
+      {/* <IconButton aria-label="notes-app">
+        <img src="src/assets/notes-app/Notes - Notes App Button.png" alt="Notes App Button"></img>
+      </IconButton> */}
 
-      </IconButton>
     </div>
   )
 }
