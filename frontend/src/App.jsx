c4331ed9df67844429f3f173b60695bfb0ce2c0f
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-01-26 22:45:44 +1100

    Added styling and functionality to open and close notes window

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,47 @@
-import IconButton from '@mui/material/IconButton'
+import { useState } from 'react'
 import './App.css'
+import Button from '@mui/material/Button'
 
 function App() {
+  const [isNotesOpen, setIsNotesOpen] = useState(false);
+
+  const viewNotes = () => {
+    if (isNotesOpen) {
+      setIsNotesOpen(false)
+    } else {
+      setIsNotesOpen(true)
+    }
+  }
 
   return (
     <div>
-      hello
-      <IconButton>
-        <img src="assets/notes/notes-app-button.png" alt="Notes App Button"></img>
-      </IconButton>
+      <div className="window-container">
+        <div id="notes-window">
+          {isNotesOpen &&
+            <img 
+              src="src/assets/notes/notes-app-background.png" 
+              alt="notes-app-background"
+              className="notes-background hidden"
+            ></img>
+          }
+          {!isNotesOpen &&
+            <img
+              src="src/assets/notes/notes-app-background.png" 
+              alt="notes-app-background"
+              className="notes-background"
+            ></img>
+          }
+        </div>
+      </div>
+      <div className="taskbar-container">
+        <Button onClick={viewNotes}>
+          <img 
+            src="src/assets/notes/notes-app-button.png" 
+            alt="Notes App Button" 
+            className="notes-button"
+          ></img>
+        </Button>
+      </div>
     </div>
   )
 }
