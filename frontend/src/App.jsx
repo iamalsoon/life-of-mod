1b4b60bf8dd2a4b63463112796c3861d2727f0de
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-01-26 21:54:08 +1100

    Fixed package issue for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import IconButton from '@mui/material/IconButton'
 import './App.css'
 
 function App() {
 
   return (
     <div>
+      hello
+      <IconButton>
+        <img src="assets/notes/notes-app-button.png" alt="Notes App Button"></img>
+      </IconButton>
     </div>
   )
 }
