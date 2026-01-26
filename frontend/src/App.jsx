d8d3a5acdfc4df525ac5fe8aa5100c4c222a2d7b
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-01-26 21:36:19 +1100

    Fixed npm packages conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
+import { IconButton } from '@mui/material'
 import './App.css'
-// import IconButton from '@mui/material';
 
 function App() {
   return (
     <div>
-      {/* <IconButton aria-label="notes-app">
-        <img src="src/assets/notes-app/Notes - Notes App Button.png" alt="Notes App Button"></img>
-      </IconButton> */}
-
+      <IconButton
     </div>
   )
 }
