9c89519c6749446212b3a2478e66b3e4b65e804d
Allyson Nguyen <z5591982@ad.unsw.edu.au>
2026-01-26 20:03:29 +1100

    Began working on notes app button functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import IconButton from '@mui/material';
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <IconButton aria-label="notes-app">
+
+      </IconButton>
+    </div>
   )
 }
 
